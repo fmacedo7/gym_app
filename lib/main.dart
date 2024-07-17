f2b439778874d2d0107a7bf86068f0d62fc231a6
Felizberto Macedo de Miranda <felizberto11fred@gmail.com>
2024-07-17 12:25:54 -0300

    add: provider as state manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:gym_app/models/workout_model.dart';
 import 'package:gym_app/pages/home_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.dark,
+    return ChangeNotifierProvider(
+      create: (_) => WorkoutModel(),
+      child: MaterialApp(
+        theme: ThemeData(
+          useMaterial3: true,
+          brightness: Brightness.dark,
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
